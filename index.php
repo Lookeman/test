b9f6881c0837d934b296ef2ca95a8a29ea862c61
Lookeman <32835963+Lookeman@users.noreply.github.com>
2017-10-17 10:23:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h1>Hello OpenShift! 2017-10-17<h1>";
+echo "Kriting:";
+echo getenv("GREETINGS");
+echo "<br>";
+
+echo "Container Name: ";
+echo gethostname();
+echo "<br>";
+echo "Client IP: ";
+echo $_SERVER["REMOTE_ADDR"];
+?>
